f22d813cd3304b3b3967cd8d08bb28094ae2e8b6
Kevin McIntosh <kmac303@gmail.com>
2022-07-11 22:02:50 -0600

    commit tree b3b48f609c91dac5c022841a093eb89f2ae3cf28

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your code here!
+// const element = document.createElement("div");
+
+// document.body.append(element);
+
+// const ul = document.createElement("ul");
+
+// for (let i = 0; i < 3; i++) {
+//     const li = document.createElement("li");
+//     li.textContent = (i + 1).toString();
+//     ul.append(li);
+//   }
+
+//   element.append(ul);
+
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = `YOUR-NAME is the champion`;
